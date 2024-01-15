a65845f9eb06aeabff323438dd73b0ff4c2d2fd6
Douglas Lowder <douglowder@mac.com>
2024-01-15 13:41:28 -0800

    Robust test code for the missing API bug

--- App.tsx
+++ App.tsx
@@ -1,38 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import * as Updates from "expo-updates";
-import { Text, TouchableHighlight, View } from "react-native";
-import colors from "tailwindcss/colors";
-
-console.log(Updates);
-
-async function handleFetchUpdate() {
-  try {
-    const update = await Updates.checkForUpdateAsync();
-
-    if (update.isAvailable) {
-      const result = await Updates.fetchUpdateAsync();
-      console.log(result);
-      await Updates.reloadAsync();
-    }
-  } catch (error) {
-    alert(`Error fetching latest Expo update: ${error}`);
-    console.log(error);
-  }
-}
+import { StatusBar } from 'expo-status-bar';
+import { checkForUpdateAsync } from 'expo-updates';
+import React from 'react';
+import { Text, TouchableHighlight, View } from 'react-native';
+import colors from 'tailwindcss/colors';
 
 export default function App() {
+  const [checkMessage, setCheckMessage] = React.useState<string>('');
+  const checkForUpdate = () => {
+    if (!checkForUpdateAsync) {
+      setCheckMessage('checkForUpdateAsync is missing');
+      return;
+    }
+    checkForUpdateAsync()
+      .then((result) => {
+        setCheckMessage(
+          `isAvailable = ${result.isAvailable}, ID = ${
+            result?.manifest?.id ?? ''
+          }`,
+        );
+      })
+      .catch((error) => {
+        setCheckMessage(`Error in checkForUpdateAsync: ${error}`);
+      });
+  };
   return (
     <View className="flex-1 items-center justify-center bg-white p-4">
       <StatusBar style="auto" />
-
+      <Text className="text-base font-semibold text-center">
+        {checkMessage}
+      </Text>
       <TouchableHighlight
         activeOpacity={1}
-        onPress={handleFetchUpdate}
+        onPress={checkForUpdate}
         underlayColor={colors.violet[700]}
         className="w-full rounded bg-violet-600 px-4 py-3"
       >
         <Text className="text-base text-white font-semibold text-center">
-          Fetch Updates
+          Check for updates
         </Text>
       </TouchableHighlight>
     </View>
